53f9c74e70fc0ba7b626addd2d1e22c746d32c16
Jungwoong Kim <dk040106@naver.com>
2021-12-26 21:16:19 +0900

    solved 2252

---
+++ src/2252.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> adj[32001], res;
+int n, m, state[32001];
+
+void dfs(int u)
+{
+    state[u] = 1;
+    for(int v : adj[u])
+    {
+        if(state[v] == 1) exit(0);
+        if(!state[v])
+        {
+            dfs(v);
+        }
+    }
+    state[u] = 2;
+    res.push_back(u);
+}
+
+void topological_sort()
+{
+    for(int s = 1; s <= n; s++)
+    {
+        if(!state[s])
+        {
+            dfs(s);
+        }
+    }
+}
+
+int main()
+{
+    cin.tie(0); cout.tie(0);
+    ios_base::sync_with_stdio(0);
+
+    cin >> n >> m;
+
+    for(int i = 0; i < m; i++)
+    {
+        int u, v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+    }
+
+    topological_sort();
+    for(auto it = res.rbegin(); it != res.rend(); it++)
+    {
+        cout << *it << ' ';
+    }
+    
+    return 0;
+}
